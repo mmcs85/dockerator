bcbeece9377ea52dcf0539db34222a1f15449877
Mário Silva <19377179+mmcs85@users.noreply.github.com>
2020-04-28 11:29:22 +0100

    little fixes

--- test/dockerator.spec.ts
+++ test/dockerator.spec.ts
@@ -7,7 +7,7 @@ class DockeratorTests {
   @test(timeout(10000)) public async runDockerfile() {
     const dock = new Dockerator({
       image: 'dfuse-eos',
-      command: ['cleos', '--help']
+      command: ['cleos', '--help'],
     })
     await dock.setup({ context: 'docker', src: ['Dockerfile'] })
     await dock.start({ blockUntilExit: true })
@@ -17,7 +17,7 @@ class DockeratorTests {
   @test public async runFalse() {
     const dock = new Dockerator({
       image: 'ubuntu:18.04',
-      command: ['bash', '-c', 'echo "Some falsehood" && false']
+      command: ['bash', '-c', 'echo "Some falsehood" && false'],
     })
     await dock.setup()
     await assert.rejects(dock.start({ blockUntilExit: true }))
@@ -27,7 +27,7 @@ class DockeratorTests {
   @test public async runError() {
     const dock = new Dockerator({
       image: 'ubuntu:18.04',
-      command: ['bash', '-c', 'cd some/non-existent/directory']
+      command: ['bash', '-c', 'cd some/non-existent/directory'],
       // stdio: 'ignore'
     })
     await dock.setup()
@@ -38,7 +38,7 @@ class DockeratorTests {
   @test public async runTrue() {
     const dock = new Dockerator({
       image: 'ubuntu:18.04',
-      command: ['bash', '-c', 'echo "Some truth" && true']
+      command: ['bash', '-c', 'echo "Some truth" && true'],
     })
     await dock.setup()
     await dock.start({ blockUntilExit: true })
@@ -48,26 +48,26 @@ class DockeratorTests {
   @test(timeout(20000)) public async runMongeMultipleTimes() {
     const dock = new Dockerator({
       image: 'mongo:4.0.6',
-      portMappings: ['27017:27017']
+      portMappings: ['27017:27017'],
     })
     await dock.setup()
     await dock.start()
-    await new Promise(resolve => setTimeout(() => resolve(), 3000))
+    await new Promise((resolve) => setTimeout(() => resolve(), 3000))
     await dock.stop({ autoRemove: false })
-    await new Promise(resolve => setTimeout(() => resolve(), 3000))
+    await new Promise((resolve) => setTimeout(() => resolve(), 3000))
     await dock.start()
-    await new Promise(resolve => setTimeout(() => resolve(), 3000))
+    await new Promise((resolve) => setTimeout(() => resolve(), 3000))
     await dock.stop()
   }
 
   @test(timeout(10000)) public async runMongo() {
     const dock = new Dockerator({
       image: 'mongo:4.0.6',
-      portMappings: ['27017:27017']
+      portMappings: ['27017:27017'],
     })
     await dock.setup()
     await dock.start()
-    await new Promise(resolve => setTimeout(() => resolve(), 6000))
+    await new Promise((resolve) => setTimeout(() => resolve(), 6000))
     await dock.stop()
   }
 }
